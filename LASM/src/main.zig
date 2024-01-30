2a0a5fb4df3dde31fc68b0ac1231badec05623c0
Abn Alsrag <85142438+AbnAlsrag@users.noreply.github.com>
2024-01-30 03:12:23 +0200

    creating the skeleton
    creating the foundation of the lasm asm

--- LASM/src/main.zig
+++ LASM/src/main.zig
@@ -1,4 +1,33 @@
 const std = @import("std");
 const lvm = @import("lvm");
 
-pub fn main() void {}
+const STACK_SIZE = 512;
+
+const Lasm = struct {
+    lvm: lvm.Machine,
+
+    fn init() Lasm {
+        var result: Lasm = Lasm{
+            .lvm = lvm.Machine.init(STACK_SIZE),
+        };
+        return result;
+    }
+
+    fn parseLasmFile(self: *Lasm, path: []const u8) void {
+        _ = self;
+        _ = path;
+    }
+
+    fn saveToMelf(self: Lasm, path: []const u8) void {
+        self.lvm.saveToFile(path);
+    }
+};
+
+pub fn main() void {
+    const in_path = "test.lasm";
+    const out_path = "test.melf";
+
+    var lasm: Lasm = Lasm.init();
+    lasm.parseLasmFile(in_path);
+    lasm.saveToMelf(out_path);
+}
